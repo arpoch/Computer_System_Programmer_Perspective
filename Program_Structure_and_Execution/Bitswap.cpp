4a86bb9d50f192ba0849c086e7ee92e420e8f6a2
Harshit <harshitchopra0712@gmail.com>
2020-05-29 18:47:56 +0530

    Output presentation change

--- Program_Structure_and_Execution/Bitswap.cpp
+++ Program_Structure_and_Execution/Bitswap.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
-void inplace_swap(int *x, int *y){
-    *y = *x ^ *y;
-    cout<<"Step_1 "<<*y<<endl;
+void inplace_swap(int *x, int *y)
+{
+    cout << "===================\n";
+    *y = *y ^ *x;
+    cout << "Step_1\n y =  " << *y << endl;
     *x = *x ^ *y;
-    cout<<"Step 2 "<<*x<<endl;
-    *y = *x ^ *y; 
-    cout<<"Step 3 "<<*y<<endl;
+    cout << "Step 2\n x = " << *x << endl;
+    *y = *x ^ *y;
+    cout << "Step 3\n y = " << *y << endl;
+    cout << "===================\n";
 }
-int main(){
-    int a =10 , b = 20;
-    cout<<"Before replace \nA="<<a<<" B="<<b<<endl;
-    inplace_swap(&a,&b);
-    cout<<"After replace \nA="<<a<<" B="<<b<<endl;
+int main()
+{
+    int a = 10, b = 20;
+    cout << "Before replace \nA=" << a << " B = " << b << endl;
+    inplace_swap(&a, &b);
+    cout << "After replace \nA=" << a << " B = " << b << endl;
     return 0;
 }
