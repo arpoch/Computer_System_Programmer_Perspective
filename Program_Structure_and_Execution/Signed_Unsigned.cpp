62c78fd81ce1cde40a2d2dd0664a499b95616b22
Harshit <harshitchopra0712@gmail.com>
2020-06-07 13:55:23 +0530

    Byte Representation of Unsigned and Signed 16/32bit

--- Program_Structure_and_Execution/Signed_Unsigned.cpp
+++ Program_Structure_and_Execution/Signed_Unsigned.cpp
@@ -14,39 +14,44 @@ void show_bytes(byte_pointer start, size_t len)
 int main()
 {
     //--------------Explicit Casting by Programmer-----------
-    short int v = -12345;
-    unsigned short uv = (unsigned short)v;
-    cout << "v = " << v << "\nuv = " << uv << '\n';
+    short int sn_16 = -12345;
+    unsigned short usn_16 = (unsigned short)sn_16;
+    cout << "sn_16 = " << sn_16 << "\nusn_16 = " << usn_16 << '\n';
     //-------------Implicit Casting by Compiler-------------
-    unsigned u = UINT32_MAX;
-    int tu = u;
-    cout << "u = " << u << "\ntu = " << tu << '\n';
+    unsigned usn_32 = UINT32_MAX;
+    int sn_32 = usn_32;
+    cout << "usn_32 = " << usn_32 << "\ntsn_32 = " << sn_32 << '\n';
     //---------------Two's Complement to Unsigned(Numeric Value)-------------------
-    short int comp = -12345;
-    unsigned short uncomp = (unsigned short)comp;
-    cout << "Implicit convertion from two's complement to unsigned = " << uncomp << '\n';
-    if (comp < 0)
+    cout << "Two's complement value (-ve) = " << sn_16 << '\n';
+    if (sn_16 < 0)
     {
-        cout << "Unsigned Numerical value = " << comp + UINT16_MAX + 1 << '\n';
+        cout << "Unsigned Numerical value by adding 2^16 to -12345= " << sn_16 + UINT16_MAX + 1 << '\n';
     }
     else
-        cout << "Unsigned Numerical value = " << uncomp << '\n';
-    //---------------Unsigned to Two's Complement(Byte Value)-------------------
-    show_bytes((byte_pointer)&comp, sizeof(short));
-    show_bytes((byte_pointer)&uncomp, sizeof(short));
-    //----------------Unsigned Value - Two's Complement-----------------
-    cout << "Unsigned Value - Two's Complement = " << uncomp - comp << '\n';
+        cout << "Unsigned Numerical value without adding 2^16 to -12345= " << usn_16 << '\n';
     //---------------Unsigned to Two's Complement(Numeric Value)-------------------
-    comp = uncomp;
-    cout << "Implicit convertion from unsigned to two's complement = " << comp << '\n';
-    if (comp > INT32_MAX)
+    unsigned short temp_16 = INT16_MAX + 120;
+    cout << "Unsigned value  = " << temp_16 << '\n';
+    if (temp_16 > INT16_MAX)
     {
-        cout << "Signed Numerical value = " << uncomp - UINT16_MAX - 1 << '\n';
+        cout << "Signed Numerical value by substracting 2^16 from 32767= " << temp_16 - UINT16_MAX - 1 << '\n';
     }
     else
-        cout << "Signed Numerical value = " << comp << '\n';
-    //---------------Unsigned to Two's Complement(Byte Value)-------------------
-    show_bytes((byte_pointer)&comp, sizeof(short));
-    show_bytes((byte_pointer)&uncomp, sizeof(short));
+        cout << "Signed Numerical value without substracting 2^16 = " << temp_16 << '\n';
+    //---------------Unsigned and Two's Complement(8Byte Value)-------------------
+    cout << "Signed Byte Representation 16bit ";
+    show_bytes((byte_pointer)&sn_16, sizeof(short));
+    cout << "Unsigned Byte Representation 16bit ";
+    show_bytes((byte_pointer)&usn_16, sizeof(unsigned short));
+    //-------------------Unsigned Byte Representation with zero extension on 32bit---------------------
+    usn_32 = usn_16;
+    sn_32 = sn_16;
+    cout << "Unsigned Byte Representation 32bit\n";
+    cout << "usn = " << usn_32 << " : ";
+    show_bytes((byte_pointer)&usn_32, sizeof(unsigned));
+    //-------------------Signed Byte Representation with signed extension on 32bit---------------------
+    cout << "Signed Byte Representation 32bit\n";
+    cout << "sn = " << sn_32 << " : ";
+    show_bytes((byte_pointer)&sn_32, sizeof(int));
     return 0;
 }
