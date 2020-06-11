523dfc01d73174df5a559d3cd1466b76a32bf9b7
Harshit <harshitchopra0712@gmail.com>
2020-06-11 10:40:54 +0530

    Turncation of Two's complement number

--- Program_Structure_and_Execution/Turncation.cpp
+++ Program_Structure_and_Execution/Turncation.cpp
@@ -6,9 +6,21 @@ int main()
 {
     cout << "Turncation\n";
     cout << "Turncation of Unsigned number \n";
-    int32_t us_val = 0xCFC7;
-    int16_t power = pow(2, 10);
+    uint32_t us_val = 0xCFC7;
+    int32_t power = pow(2, 10);
     cout << "Value of us_val = " << us_val << '\n';
     cout << "Turncation of us_val upto 10bits = " << (us_val % power) << '\n';
+    cout << "Turncation of Signed number\n";
+    int32_t s_val = 0xCFC7;
+    power = pow(2, 16);
+    cout << "Value of us_val = " << s_val << '\n';
+    if ((s_val % power) > INT16_MAX)
+    {
+        cout << "Turncation of s_val upto 16bits = " << (s_val % power) - (2 * INT16_MAX + 2) << '\n';
+    }
+    else
+    {
+        cout << "Turncation of s_val upto 16bits = " << (s_val % power) << '\n';
+    }
     return 0;
 }
