4eade8cd2b5e35714f6a3e7734c0ac9923c4c8d9
Harshit <harshitchopra0712@gmail.com>
2020-06-16 23:36:48 +0530

    Program files in cpp

---
+++ Program_Structure_and_Execution/Chapter 3:Machine Representation of Program/Program Encoding/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+
+void multstore(long, long, long *);
+int main()
+{
+    long d;
+    multstore(2, 3, &d);
+    printf("2 * 3 --> %ld\n", d);
+    return 0;
+}
+long mult2(long a, long b)
+{
+    long s = a * b;
+    return s;
+}
